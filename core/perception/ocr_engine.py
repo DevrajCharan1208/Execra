bcb76607373e185b724e2bc72ba74d2da468c4b0
yashashwani_dixit <yashashwanidixit@gmail.com>
2026-05-13 17:36:42 +0530

    digital ocr working fine...physical ocr: min_conf=70 and psm 6 config

---
+++ core/perception/ocr_engine.py
@@ -0,0 +1,103 @@
+import pytesseract
+import numpy as np
+from PIL import Image
+import cv2#i have added cv2 dependency for grayscaling
+import re
+
+
+
+
+
+class OCREngine:
+    def __init__(self,language="eng"):
+        self.language=language
+
+
+    def validate_frame(self,array:np.ndarray):
+    
+        if array is None:
+            raise ValueError("THis array is not allowed.")    
+
+        if not isinstance(array, np.ndarray):
+            raise ValueError("Frame must be a numpy ndarray")    
+        
+
+
+
+        if array.size==0:
+            return False
+        else :
+            return True    
+    def convert_to_pil_image(self,array:np.ndarray):
+        
+        return Image.fromarray(array)
+
+        
+
+
+    def extract_physical_text_with_boxes(self,array:np.ndarray,min_con:int=70)  :
+        
+        valid=self.validate_frame(array)
+        if not valid :
+            return []
+
+        gray=cv2.cvtColor(array,cv2.COLOR_BGR2GRAY)
+
+
+    
+        denoised=cv2.fastNlMeansDenoising(gray, h=10)
+        processed = cv2.adaptiveThreshold(
+            denoised, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 
+            cv2.THRESH_BINARY, 11, 2
+
+
+    )
+        pil_image=self.convert_to_pil_image(processed)
+        data = pytesseract.image_to_data(pil_image, output_type=pytesseract.Output.DICT,config='--oem 3 --psm 6')
+        
+        
+        final=[]
+        for i in range(len(data['text'])):
+            
+            text=data['text'][i].strip()
+    
+
+
+            if data["conf"][i]>=min_con and text=="" :
+                continue
+            try :
+                conf=float(data["conf"][i])
+            except ValueError:#to prevent lines and words..cf level=-1
+                continue
+           
+
+
+            if conf>=min_con and text!="":
+                final.append({
+                    "text":text,
+                    "box":{
+                        "left":data["left"][i],
+                        
+                        "top":data["top"][i],
+                        "height":data["height"][i],
+                        "width":data["width"][i]
+
+                    },
+                    "conf":conf
+                })
+        return final 
+
+
+    def extract_dig_text(self,array:np.ndarray)->str:
+        valid=self.validate_frame(array)
+        if not valid:
+            return ""
+
+
+        gray=cv2.cvtColor(array,cv2.COLOR_BGR2GRAY)    
+
+        pil_img=self.convert_to_pil_image(gray)
+        return pytesseract.image_to_string(pil_img,lang=self.language,config="--oem 3 --psm 6").strip()
+                   
+
+          
